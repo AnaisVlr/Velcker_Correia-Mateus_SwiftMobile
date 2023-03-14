16818981e8766de08173c79de64dd9a6d6c65e77
Dokarus <dorian.752@live.fr>
2023-03-14 10:59:00 +0100

    Avec version xcode 13

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by m1 on 14/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/Velcker_Correia_Mateus_SwiftMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Velcker_Correia_Mateus_SwiftMobileApp.swift
+//  Shared
+//
+//  Created by m1 on 14/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Velcker_Correia_Mateus_SwiftMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
