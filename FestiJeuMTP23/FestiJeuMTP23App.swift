61f5a530b7d86ffd5325c2ca4e5f6ec10b31d7d4
etud <etud@MacBook-Pro-de-cadmin.local>
2023-03-14 09:50:41 +0100

    Lancement swiftApp

---
+++ FestiJeuMTP23/FestiJeuMTP23App.swift
@@ -0,0 +1,17 @@
+//
+//  FestiJeuMTP23App.swift
+//  FestiJeuMTP23
+//
+//  Created by etud on 14/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct FestiJeuMTP23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
