aab078ec7de44599f9232f170654b474819dfa8d
Dokarus <dorian.752@live.fr>
2023-03-14 11:17:46 +0100

    On recommence

---
+++ Velcker_Correia-Mateus_SwiftMobile/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Velcker_Correia-Mateus_SwiftMobile
+//
+//  Created by m1 on 14/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world Dorian!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
