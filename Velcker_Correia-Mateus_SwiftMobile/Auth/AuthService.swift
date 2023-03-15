1f58c26a44fa5acabb28a87e75d1ac4f4f8fa4cd
Dokarus <dorian.752@live.fr>
2023-03-15 22:53:32 +0100

    La connexion marche presques

--- Velcker_Correia-Mateus_SwiftMobile/Auth/AuthService.swift
+++ Velcker_Correia-Mateus_SwiftMobile/Auth/AuthService.swift
@@ -6,7 +6,30 @@
 //
 import Foundation
 
+struct AuthDTO : Decodable {
+  var access_token: String
+}
+
 class AuthService{
-    private static let authUrl = URL(string: "https://velcker-correia-mateus-api-mobile.cluster-ig3.igpolytech.fr/auth")!
+  private static let authUrl = URL(string: "https://velcker-correia-mateus-api-mobile.cluster-ig3.igpolytech.fr/auth")!
+  
+  static public func tryConnect(id: String, pwd: String) async {
+    let jsonString = "{ \"email_benevole\": \(id), \"password_benevole\": \(pwd) }"
+    guard let jsonData = jsonString.data(using: .utf8) else {return}
     
+    var request = URLRequest(url: self.authUrl)
+    request.httpMethod = "POST"
+    request.httpBody = jsonData
+    request.setValue("application/json", forHTTPHeaderField: "Content-type")
+    //request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+    
+    do{
+      let (data, _) = try await URLSession.shared.upload(for: request, from: jsonData)
+      print("Anais au secours ")
+      guard let decoded : AuthDTO = await JSONHelper.decode(data: data) else {print("marche pô"); return}
+      print(decoded.access_token)
+    }catch{
+      print("Erreur lors de la connexion")
+    }
+  }
 }
--- Velcker_Correia-Mateus_SwiftMobile/View/AuthView.swift
+++ Velcker_Correia-Mateus_SwiftMobile/View/AuthView.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct AuthView: View {
-    var body: some View {
-        Text("Salut Anaïs")
-            .padding()
+  @State var identifiant : String = "dorian.752@live.fr"
+  @State var password : String = "123"
+  
+  var body: some View {
+    VStack(alignment: .center) {
+      
+      Text("Connexion")
+        .padding()
+      
+      VStack(alignment: .center){
+        TextField("", text: $identifiant)
+          .textFieldStyle(.roundedBorder)
+        SecureField("", text: $password)
+          .textFieldStyle(.roundedBorder)
+        
+        Button {
+          Task {
+            let result: () = await AuthService.tryConnect(id: identifiant, pwd: password)
+            print(result)
+          }
+         
+        } label: {
+          Text("Se connecter")
+        }
+        .padding()
+        .background(Color(red:0, green: 0, blue: 0.5))
+        .clipShape(Capsule())
+      }.padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
     }
+  }
 }
