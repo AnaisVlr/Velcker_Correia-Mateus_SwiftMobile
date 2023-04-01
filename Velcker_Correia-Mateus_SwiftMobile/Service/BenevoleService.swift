c6f1353056a6dd8afc5c977210308c509aaf66de
Dokarus <dorian.752@live.fr>
2023-04-01 13:08:18 +0200

    Modifie la tabView d'un festival

--- Velcker_Correia-Mateus_SwiftMobile/Service/BenevoleService.swift
+++ Velcker_Correia-Mateus_SwiftMobile/Service/BenevoleService.swift
@@ -49,8 +49,7 @@ class BenevoleService{
       guard let data = data, error == nil else{
         return completion(.failure(ServiceError.NoData))
       }
-        let dataString = String(data: data, encoding: .utf8)!
-        print(dataString)
+      
       Task{
         do{
           let decoded : BenevoleDTO? = await JSONHelper.decode(data: data)
